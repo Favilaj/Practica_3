d7312e3f28dacdd4289cfbb1f3a8334ffb533651
WIN10PRO <WIN10PRO@DESKTOP-34KC35S>
2022-09-22 23:57:46 -0700

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practica_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int lado;
+
+            Console.WriteLine("Favor de Ingresar Lado: ");
+            lado = int.Parse(Console.ReadLine());
+
+            Console.Write("El perimetro del poligono regular es: " + lado * 4);
+
+            Console.ReadKey();
+        }
+    }
+}
